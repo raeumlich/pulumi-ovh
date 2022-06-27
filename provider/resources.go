c309075e79a70dfe9ca3533d2b368a7596b0c180
Daniel Trautmann <daniel.trautmann@raeumlich.studio>
2022-06-27 16:17:41 +0200

    Initial commit

--- provider/resources.go
+++ provider/resources.go
@@ -12,27 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package xyz
+package ovh
 
 import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/ovh/terraform-provider-ovh/ovh"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/terraform-providers/terraform-provider-xyz/xyz"
+	"github.com/raeumlich/pulumi-ovh/provider/pkg/version"
 )
 
 // all of the token components used below.
 const (
 	// This variable controls the default name of the package in the package
 	// registries for nodejs and python:
-	mainPkg = "xyz"
+	mainPkg = "ovh"
 	// modules:
-	mainMod = "index" // the xyz module
+	mainMod = "index" // the ovh module
 )
 
 // preConfigureCallback is called before the providerConfigure function of the underlying provider.
@@ -46,12 +46,12 @@ func preConfigureCallback(vars resource.PropertyMap, c shim.ResourceConfig) erro
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(xyz.Provider())
+	p := shimv2.NewProvider(ovh.Provider())
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
 		P:    p,
-		Name: "xyz",
+		Name: "ovh",
 		// DisplayName is a way to be able to change the casing of the provider
 		// name when being displayed on the Pulumi registry
 		DisplayName: "",
@@ -70,16 +70,16 @@ func Provider() tfbridge.ProviderInfo {
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
 		PluginDownloadURL: "",
-		Description:       "A Pulumi package for creating and managing xyz cloud resources.",
+		Description:       "A Pulumi package for creating and managing ovh cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
 		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
-		Keywords:   []string{"pulumi", "xyz", "category/cloud"},
+		Keywords:   []string{"pulumi", "ovh", "category/cloud"},
 		License:    "Apache-2.0",
 		Homepage:   "https://www.pulumi.com",
-		Repository: "https://github.com/pulumi/pulumi-xyz",
+		Repository: "https://github.com/raeumlich/pulumi-ovh",
 		// The GitHub Org for the provider - defaults to `terraform-providers`
-		GitHubOrg: "",
+		GitHubOrg: "ovh",
 		Config:    map[string]*tfbridge.SchemaInfo{
 			// Add any required configuration here, or remove the example below if
 			// no additional points are required.
@@ -91,7 +91,7 @@ func Provider() tfbridge.ProviderInfo {
 			// },
 		},
 		PreConfigureCallback: preConfigureCallback,
-		Resources:            map[string]*tfbridge.ResourceInfo{
+		Resources: map[string]*tfbridge.ResourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi type. Two examples
 			// are below - the single line form is the common case. The multi-line form is
 			// needed only if you wish to override types or other default options.
@@ -104,11 +104,266 @@ func Provider() tfbridge.ProviderInfo {
 			// 		"tags": {Type: tfbridge.MakeType(mainPkg, "Tags")},
 			// 	},
 			// },
+			"ovh_cloud_project": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProject"),
+			},
+			"ovh_cloud_project_containerregistry": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectContainerRegistry"),
+			},
+			"ovh_cloud_project_containerregistry_user": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectContainerRegistryUser"),
+			},
+			"ovh_cloud_project_failover_ip_attach": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectFailoverIPAttach"),
+			},
+			"ovh_cloud_project_kube": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectKube"),
+			},
+			"ovh_cloud_project_kube_nodepool": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectKubeNodepool"),
+			},
+			"ovh_cloud_project_network_private": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectNetworkPrivate"),
+			},
+			"ovh_cloud_project_network_private_subnet": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectNetworkPrivateSubnet"),
+			},
+			"ovh_cloud_project_user": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "CloudProjectUser"),
+			},
+			"ovh_dbaas_logs_input": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DBaaSLogsInput"),
+			},
+			"ovh_dbaas_logs_output_graylog_stream": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DBaaSLogsOutputGraylogStream"),
+			},
+			"ovh_dedicated_ceph_acl": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DedicatedCephACL"),
+			},
+			"ovh_dedicated_server_install_task": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DedicatedServerInstallTask"),
+			},
+			"ovh_dedicated_server_reboot_task": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DedicatedServerRebootTask"),
+			},
+			"ovh_dedicated_server_update": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DedicatedServerUpdate"),
+			},
+			"ovh_domain_zone": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DomainZone"),
+			},
+			"ovh_domain_zone_record": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DomainZoneRecord"),
+			},
+			"ovh_domain_zone_redirection": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "DomainZoneRedirection"),
+			},
+			"ovh_ip_reverse": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPReverse"),
+			},
+			"ovh_ip_service": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPService"),
+			},
+			"ovh_iploadbalancing": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancing"),
+			},
+			"ovh_iploadbalancing_http_farm": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingHTTPFarm"),
+			},
+			"ovh_iploadbalancing_http_farm_server": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingHTTPFarmServer"),
+			},
+			"ovh_iploadbalancing_http_frontend": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingHTTPFrontend"),
+			},
+			"ovh_iploadbalancing_http_route": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingHTTPRoute"),
+			},
+			"ovh_iploadbalancing_http_route_rule": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingHTTPRouteRule"),
+			},
+			"ovh_iploadbalancing_refresh": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingRefresh"),
+			},
+			"ovh_iploadbalancing_tcp_farm": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingTCPFarm"),
+			},
+			"ovh_iploadbalancing_tcp_farm_server": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingTCPFarmServer"),
+			},
+			"ovh_iploadbalancing_tcp_frontend": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingTCPFrontend"),
+			},
+			"ovh_iploadbalancing_tcp_route": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingTCPRoute"),
+			},
+			"ovh_iploadbalancing_tcp_route_rule": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingTCPRouteRule"),
+			},
+			"ovh_iploadbalancing_vrack_network": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPLoadBalancingVRackNetwork"),
+			},
+			"ovh_me_identity_user": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeIdentityUser"),
+			},
+			"ovh_me_installation_template": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeInstallationTemplate"),
+			},
+			"ovh_me_installation_template_partition_scheme": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeInstallationTemplatePartitionScheme"),
+			},
+			"ovh_me_installation_template_partition_scheme_hardware_raid": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeInstallationTemplatePartitionSchemeHardwareRAID"),
+			},
+			"ovh_me_installation_template_partition_scheme_partition": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeInstallationTemplatePartitionSchemePartition"),
+			},
+			"ovh_me_ipxe_script": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "IPXEScript"),
+			},
+			"ovh_me_ssh_key": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "MeSSHKey"),
+			},
+			"ovh_vrack": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRack"),
+			},
+			"ovh_vrack_cloudproject": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRackCloudProject"),
+			},
+			"ovh_vrack_dedicated_server": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRackDedicatedServer"),
+			},
+			"ovh_vrack_dedicated_server_interface": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRackDedicatedServerInterface"),
+			},
+			"ovh_vrack_ip": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRackIP"),
+			},
+			"ovh_vrack_iploadbalancing": {
+				Tok: tfbridge.MakeResource(mainPkg, mainMod, "VRackIPLoadBalancing"),
+			},
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi function. An example
 			// is below.
 			// "aws_ami": {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAmi")},
+			"ovh_cloud_project_capabilities_containerregistry": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectCapabilitiesContainerRegistry"),
+			},
+			"ovh_cloud_project_capabilities_containerregistry_filter": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectCapabilitiesContainerregistryFilter"),
+			},
+			"ovh_cloud_project_containerregistries": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectContainerRegistries"),
+			},
+			"ovh_cloud_project_containerregistry": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectContainerregistry"),
+			},
+			"ovh_cloud_project_containerregistry_users": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectContainerRegistryUsers"),
+			},
+			"ovh_cloud_project_failover_ip_attach": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectFailoverIPAttach"),
+			},
+			"ovh_cloud_project_kube": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectKube"),
+			},
+			"ovh_cloud_project_region": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectRegion"),
+			},
+			"ovh_cloud_project_regions": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCloudProjectRegions"),
+			},
+			"ovh_dbaas_logs_input_engine": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDBaaSLogsInputEngine"),
+			},
+			"ovh_dbaas_logs_output_graylog_stream": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDBaaSLogsOutputGraylogStream"),
+			},
+			"ovh_dedicated_ceph": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDedicatedCeph"),
+			},
+			"ovh_dedicated_installation_templates": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDedicatedInstallationTemplates"),
+			},
+			"ovh_dedicated_server": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDedicatedServer"),
+			},
+			"ovh_dedicated_server_boots": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDedicatedServerBoots"),
+			},
+			"ovh_dedicated_servers": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDedicatedServers"),
+			},
+			"ovh_domain_zone": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDomainZone"),
+			},
+			"ovh_ip_service": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getIPService"),
+			},
+			"ovh_iploadbalancing": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getIPLoadBalancing"),
+			},
+			"ovh_iploadbalancing_vrack_network": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getIPLoadBalancingVRackNetwork"),
+			},
+			"ovh_iploadbalancing_vrack_networks": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getIPLoadBalancingVRackNetworks"),
+			},
+			"ovh_me": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMe"),
+			},
+			"ovh_me_identity_user": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeIdentityUser"),
+			},
+			"ovh_me_identity_users": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeIdentityUsers"),
+			},
+			"ovh_me_installation_template": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeInstallationTemplate"),
+			},
+			"ovh_me_installation_templates": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeInstallationTemplates"),
+			},
+			"ovh_me_ipxe_script": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeIPXEScript"),
+			},
+			"ovh_me_ipxe_scripts": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeIPXEScripts"),
+			},
+			"ovh_me_paymentmean_bankaccount": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMePaymentMeanBankAccount"),
+			},
+			"ovh_me_paymentmean_creditcard": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMePaymentMeanCreditCard"),
+			},
+			"ovh_me_ssh_key": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeSSHKey"),
+			},
+			"ovh_me_ssh_keys": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMeSSHKeys"),
+			},
+			"ovh_order_cart": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getOrderCart"),
+			},
+			"ovh_order_cart_product": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getOrderCartProduct"),
+			},
+			"ovh_order_cart_product_options": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getOrderCartProductOptions"),
+			},
+			"ovh_order_cart_product_options_plan": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getOrderCartProductOptionsPlan"),
+			},
+			"ovh_order_cart_product_plan": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getProductCartProductPlan"),
+			},
+			"ovh_vps": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getVPS"),
+			},
+			"ovh_vracks": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getVRacks"),
+			},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			// List any npm dependencies and their versions
